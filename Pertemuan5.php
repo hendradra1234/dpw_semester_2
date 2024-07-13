0da2dbcf0bacdc373ba51f25f654c642851fbe8a
hendra-hp-victus <hendra@artamedianet.co.id>
2024-07-14 01:05:58 +0700

    refactor code

--- Pertemuan5.php
+++ Pertemuan5.php
@@ -3,17 +3,17 @@
 
 <h3>Program 5.1</h3>
 <?php 
-$arryBuah = array ("Mangga", "Apple", "Pisang", "Jeruk");
-echo "$arryBuah[1]<br>";
-echo "$arryBuah[2]<br>";
+    $arryBuah = array ("Mangga", "Apple", "Pisang", "Jeruk");
+    echo "$arryBuah[1]<br>";
+    echo "$arryBuah[2]<br>";
 
-$arryWarna[] = "red";
-$arryWarna[] = "blue";
-$arryWarna[] = "yellow";
-$arryWarna[] = "grey";
+    $arryWarna[] = "red";
+    $arryWarna[] = "blue";
+    $arryWarna[] = "yellow";
+    $arryWarna[] = "grey";
 
-echo "<div style = 'background-color:$arryWarna[1]'>$arryWarna[1]</div><br>";
-echo "<div style = 'background-color:$arryWarna[3]'>$arryWarna[3]</div><br>";
+    echo "<div style = 'background-color:$arryWarna[1]'>$arryWarna[1]</div><br>";
+    echo "<div style = 'background-color:$arryWarna[3]'>$arryWarna[3]</div><br>";
 ?>
 <h3>Program 5.2</h3>
 <?php 
--- readme.md
+++ readme.md
@@ -7,3 +7,6 @@
 - Latihan 2
 - Pertemuan 3
 - Pertemuan 4
+- Pertemuan 5
+- Pertemuan 6
+- Latihan Pertemuan 7
